3f6acc27788e6ddb2ee297f14ce90726d35f25f8
gaoqing <1300610479@qq.com>
2024-06-28 00:43:55 +0900

    2024/6/10   ？？最终定版
    Signed-off-by: gaoqing <1300610479@qq.com>

---
+++ src/test/java/com/gaoqing/gaoqingblog/GaoqingBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaoqing.gaoqingblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaoqingBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
